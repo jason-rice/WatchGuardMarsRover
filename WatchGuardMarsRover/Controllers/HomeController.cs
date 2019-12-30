8f9db35b36564742b9b36b30eb703d697b812579
Jason Rice <jasonmatthew2352@gmail.com>
2019-12-29 22:41:49 -0600

    ASP.Net Core MVC app, gets images by date from Nasa Mars Rover Photos API. Can either select a date to get images from, or can select dates from dates.txt file stored locally. The images are stored locally, then displayed in web browser.

--- WatchGuardMarsRover/Controllers/HomeController.cs
+++ WatchGuardMarsRover/Controllers/HomeController.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using WatchGuardMarsRover.Helper;
 using WatchGuardMarsRover.Models;
-//using static System.Net.Mime.MediaTypeNames;
-using System.Drawing;
 using Microsoft.AspNetCore.Http;
 
 namespace WatchGuardMarsRover.Controllers
@@ -20,11 +12,13 @@ namespace WatchGuardMarsRover.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        MarsRoverApi _api = new MarsRoverApi();
+        private MarsRoverApi _api = new MarsRoverApi();
+        protected Repository _repository { get; set; }
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _repository = new Repository();
         }
 
         public IActionResult Index()
@@ -36,35 +30,31 @@ namespace WatchGuardMarsRover.Controllers
         {
             if(dateChosen != null)
             {
-                try
-                {
-                    MarsRoverImage marsRoverImage = new MarsRoverImage();
-                    HttpClient client = _api.Initial();
-                    //dateChosen = "2015-6-3";
-                    HttpResponseMessage responseMessage = await client.GetAsync("api/v1/rovers/curiosity/photos?earth_date=" + dateChosen + "&api_key=gVnoxQADOqBDAVopaZlfEhYtL7gNSxrXrQDruJDv");
-                    if (responseMessage.IsSuccessStatusCode)
-                    {
-                        var resultString = responseMessage.Content.ReadAsStringAsync().Result;
-                        marsRoverImage = JsonConvert.DeserializeObject<MarsRoverImage>(resultString);
-
-                        foreach (var item in marsRoverImage.PhotoList)
-                        {
-                            using (WebClient currentClient = new WebClient())
-                            {
-                                currentClient.DownloadFile(new Uri(item.img_src), @"C:\Users\jason\source\repos\WatchGuardMarsRover\WatchGuardMarsRover\wwwroot\Images\NasaPic" + item.id + ".png");
-                            }
-                        }
-                    }
-                    return Json("success");
-                }
-                catch
-                {
-                    return Json("fail");
-                }
+                List<DisplayImages> displayImagesList = new List<DisplayImages>();
+                await _repository.GetNasaImagesByDate(dateChosen, displayImagesList);
+                return Json(displayImagesList);
             }
             else
             {
-                return Json("fail");
+                List<DisplayImages> displayImagesList = new List<DisplayImages>();
+                await _repository.GetNasaImagesByDateArray(displayImagesList);
+                return Json(displayImagesList);
+            }
+        }
+
+        public async Task<IActionResult> GetFirstFifteenImagesByDate(string dateChosen)
+        {
+            if (dateChosen != null)
+            {
+                List<DisplayImages> displayImagesList = new List<DisplayImages>();
+                await _repository.GetFirstFifteenNasaImagesByDate(dateChosen, displayImagesList);
+                return Json(displayImagesList);
+            }
+            else
+            {
+                List<DisplayImages> displayImagesList = new List<DisplayImages>();
+                await _repository.GetFirstFifteenNasaImagesByDateArray(displayImagesList);
+                return Json(displayImagesList);
             }
         }
 
---
+++ WatchGuardMarsRover/Helper/Repository.cs
@@ -0,0 +1,176 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WatchGuardMarsRover.Models;
+
+namespace WatchGuardMarsRover.Helper
+{
+    public class Repository
+    {
+        MarsRoverApi _api = new MarsRoverApi();
+
+        public async Task<List<DisplayImages>> GetNasaImagesByDate(string dateChosen, List<DisplayImages> displayImagesList)
+        {
+            try
+            {
+                MarsRoverImage marsRoverImage = new MarsRoverImage();
+                HttpClient client = _api.Initial();
+                HttpResponseMessage responseMessage = await client.GetAsync("api/v1/rovers/curiosity/photos?earth_date=" + dateChosen + "&api_key=gVnoxQADOqBDAVopaZlfEhYtL7gNSxrXrQDruJDv");
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var resultString = responseMessage.Content.ReadAsStringAsync().Result;
+                    marsRoverImage = JsonConvert.DeserializeObject<MarsRoverImage>(resultString);
+
+                    foreach (var item in marsRoverImage.PhotoList)
+                    {
+                        try
+                        {
+                            using (WebClient currentClient = new WebClient())
+                            {
+                                currentClient.DownloadFile(new Uri(item.img_src), Path.GetFullPath("wwwroot/Images/NasaPic" + item.id + ".png"));
+                                DisplayImages displayImages = new DisplayImages();
+                                displayImages.ImageName = "NasaPic" + item.id + ".png";
+                                displayImagesList.Add(displayImages);
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+                return displayImagesList;
+            }
+            catch
+            {
+                return displayImagesList;
+            }
+        }
+
+        public async Task<List<DisplayImages>> GetNasaImagesByDateArray(List<DisplayImages> displayImagesList)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(Path.GetFullPath("wwwroot/TextFile/dates.txt")))
+                {
+                    string line = sr.ReadToEnd();
+                    string[] dateStringArray = line.Split("\n");
+
+                    foreach (var item in dateStringArray)
+                    {
+                        try
+                        {
+                            DateTime newDateChosen = new DateTime();
+                            if (Convert.ToDateTime(item) != null)
+                            {
+                                newDateChosen = Convert.ToDateTime(item);
+                                string newDateChosenString = newDateChosen.Year + "-" + newDateChosen.Month + "-" + newDateChosen.Day;
+                                await GetNasaImagesByDate(newDateChosenString, displayImagesList);
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+
+                    }
+                }
+                return displayImagesList;
+            }
+            catch
+            {
+                return displayImagesList;
+            }
+        }
+
+
+        public async Task<List<DisplayImages>> GetFirstFifteenNasaImagesByDate(string dateChosen, List<DisplayImages> displayImagesList)
+        {
+            try
+            {
+                MarsRoverImage marsRoverImage = new MarsRoverImage();
+                HttpClient client = _api.Initial();
+                HttpResponseMessage responseMessage = await client.GetAsync("api/v1/rovers/curiosity/photos?earth_date=" + dateChosen + "&api_key=gVnoxQADOqBDAVopaZlfEhYtL7gNSxrXrQDruJDv");
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var resultString = responseMessage.Content.ReadAsStringAsync().Result;
+                    marsRoverImage = JsonConvert.DeserializeObject<MarsRoverImage>(resultString);
+                    int count = 0;
+
+                    foreach (var item in marsRoverImage.PhotoList)
+                    {
+                        if (count < 5)
+                        {
+                            try
+                            {
+                                using (WebClient currentClient = new WebClient())
+                                {
+                                    currentClient.DownloadFile(new Uri(item.img_src), Path.GetFullPath("wwwroot/Images/NasaPic" + item.id + ".png"));
+                                    DisplayImages displayImages = new DisplayImages();
+                                    displayImages.ImageName = "NasaPic" + item.id + ".png";
+                                    displayImagesList.Add(displayImages);
+                                }
+                            }
+                            catch
+                            {
+
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        count++;
+                    }
+                }
+                return displayImagesList;
+            }
+            catch
+            {
+                return displayImagesList;
+            }
+        }
+
+        public async Task<List<DisplayImages>> GetFirstFifteenNasaImagesByDateArray(List<DisplayImages> displayImagesList)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(Path.GetFullPath("wwwroot/TextFile/dates.txt")))
+                {
+                    string line = sr.ReadToEnd();
+                    string[] dateStringArray = line.Split("\n");
+
+                    foreach (var item in dateStringArray)
+                    {
+                        try
+                        {
+                            DateTime newDateChosen = new DateTime();
+                            if (Convert.ToDateTime(item) != null)
+                            {
+                                newDateChosen = Convert.ToDateTime(item);
+                                string newDateChosenString = newDateChosen.Year + "-" + newDateChosen.Month + "-" + newDateChosen.Day;
+                                await GetFirstFifteenNasaImagesByDate(newDateChosenString, displayImagesList);
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+
+                    }
+                }
+                return displayImagesList;
+            }
+            catch
+            {
+                return displayImagesList;
+            }
+        }
+
+
+    }
+}
